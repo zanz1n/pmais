dae1366187ed54eb986df9b08fe69f674ec4515d
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-04-06 22:37:42 -0300

    implementing logging to tauri application

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,7 @@ mod kvstore;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::default().build())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
