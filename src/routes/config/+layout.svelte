095cf0571fbb543cda7fecf3362d57988a44ff35
Gustavo Henrique <gustavohsg2@hotmail.com>
2024-04-06 23:30:46 -0300

    Rotas v1
    Rotas para as demais páginas funcionando.

---
+++ src/routes/config/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import LeftRail from "../../components/LeftRail.svelte";
+    import TopBar from "../../components/TopBar.svelte";
+
+    const route = ["Config"];
+</script>
+
+<div class="flex flex-col h-full w-full">
+    <TopBar {route} />
+    <div class="flex flex-row h-full w-full">
+        <LeftRail route="config" />
+        <slot />
+    </div>
+</div>
---
+++ src/routes/saep/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import LeftRail from "../../components/LeftRail.svelte";
+    import TopBar from "../../components/TopBar.svelte";
+
+    const route = ["Simulado"];
+</script>
+
+<div class="flex flex-col h-full w-full">
+    <TopBar {route} />
+    <div class="flex flex-row h-full w-full">
+        <LeftRail route="saep" />
+        <slot />
+    </div>
+</div>
